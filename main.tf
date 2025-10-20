39933a7edeac47cedbf5cc12624bbadfff55418d
Nadav <105583152+TheNadav@users.noreply.github.com>
2025-10-20 15:11:26 +0300

    Add intentionally vulnerable AWS resources for testing purposes

--- main.tf
+++ main.tf
@@ -159,3 +159,218 @@ resource "aws_cloudtrail" "vulnerable_trail" {
 
   is_multi_region_trail = false
 }
+
+resource "aws_lambda_function" "vulnerable_lambda" {
+  filename      = "lambda.zip"
+  function_name = "vulnerable-function"
+  role          = aws_iam_role.vulnerable_role.arn
+  handler       = "index.handler"
+  runtime       = "python3.9"
+
+  environment {
+    variables = {
+      DB_PASSWORD = "supersecret123"
+      API_KEY     = "sk-1234567890abcdef"
+    }
+  }
+
+  tracing_config {
+    mode = "PassThrough"
+  }
+}
+
+resource "aws_elasticsearch_domain" "vulnerable_es" {
+  domain_name           = "vulnerable-elasticsearch"
+  elasticsearch_version = "7.10"
+
+  cluster_config {
+    instance_type = "t3.small.elasticsearch"
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
+
+  encrypt_at_rest {
+    enabled = false
+  }
+
+  node_to_node_encryption {
+    enabled = false
+  }
+
+  domain_endpoint_options {
+    enforce_https = false
+  }
+}
+
+resource "aws_redshift_cluster" "vulnerable_redshift" {
+  cluster_identifier = "vulnerable-redshift"
+  database_name      = "mydb"
+  master_username    = "admin"
+  master_password    = "Password123"
+  node_type          = "dc2.large"
+  cluster_type       = "single-node"
+
+  encrypted                  = false
+  publicly_accessible        = true
+  skip_final_snapshot        = true
+  automated_snapshot_retention_period = 0
+
+  logging {
+    enable = false
+  }
+}
+
+resource "aws_eks_cluster" "vulnerable_eks" {
+  name     = "vulnerable-cluster"
+  role_arn = aws_iam_role.vulnerable_role.arn
+
+  vpc_config {
+    endpoint_private_access = false
+    endpoint_public_access  = true
+    public_access_cidrs     = ["0.0.0.0/0"]
+  }
+
+  encryption_config {
+    resources = ["secrets"]
+    provider {
+      key_arn = ""
+    }
+  }
+
+  enabled_cluster_log_types = []
+}
+
+resource "aws_kms_key" "vulnerable_kms" {
+  description             = "Vulnerable KMS key"
+  deletion_window_in_days = 7
+  enable_key_rotation     = false
+}
+
+resource "aws_dynamodb_table" "vulnerable_dynamodb" {
+  name           = "vulnerable-table"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  point_in_time_recovery {
+    enabled = false
+  }
+
+  server_side_encryption {
+    enabled = false
+  }
+}
+
+resource "aws_efs_file_system" "vulnerable_efs" {
+  creation_token = "vulnerable-efs"
+  encrypted      = false
+}
+
+resource "aws_api_gateway_rest_api" "vulnerable_api" {
+  name        = "vulnerable-api"
+  description = "Vulnerable API Gateway"
+}
+
+resource "aws_api_gateway_method" "vulnerable_method" {
+  rest_api_id   = aws_api_gateway_rest_api.vulnerable_api.id
+  resource_id   = aws_api_gateway_rest_api.vulnerable_api.root_resource_id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_sns_topic" "vulnerable_sns" {
+  name = "vulnerable-topic"
+}
+
+resource "aws_sqs_queue" "vulnerable_sqs" {
+  name = "vulnerable-queue"
+}
+
+resource "aws_ecr_repository" "vulnerable_ecr" {
+  name                 = "vulnerable-repo"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "aws_elb" "vulnerable_elb" {
+  name               = "vulnerable-elb"
+  availability_zones = ["us-east-1a", "us-east-1b"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = false
+  idle_timeout                = 400
+  connection_draining         = false
+}
+
+resource "aws_network_acl" "vulnerable_nacl" {
+  vpc_id = aws_vpc.vulnerable_vpc.id
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+}
+
+resource "aws_vpc" "vulnerable_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_subnet" "vulnerable_subnet" {
+  vpc_id                  = aws_vpc.vulnerable_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_secretsmanager_secret" "vulnerable_secret" {
+  name = "vulnerable-secret"
+  recovery_window_in_days = 0
+}
+
+resource "aws_secretsmanager_secret_version" "vulnerable_secret_value" {
+  secret_id     = aws_secretsmanager_secret.vulnerable_secret.id
+  secret_string = jsonencode({
+    password = "hardcoded-password-in-code"
+    api_key  = "1234567890"
+  })
+}
